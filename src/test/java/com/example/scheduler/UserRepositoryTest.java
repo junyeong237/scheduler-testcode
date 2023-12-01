815a92ac144715ffb74fc97d009c2ea9c85825e2
juneyoung Lee <junyeong237@gmail.com>
2023-12-02 02:30:13 +0900

    Feat : UserRepository 테스트 추가

---
+++ src/test/java/com/example/scheduler/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.scheduler;
+
+import com.example.scheduler.entity.User;
+import com.example.scheduler.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("UserRepository 테스트")
+    void findByUsername_ExistingUser_ShouldReturnUser() {
+        // Given
+        String username = "park";
+        User user = User.builder().username(username).password("password").build();
+        userRepository.save(user);
+
+        // When
+        Optional<User> resultUser = userRepository.findByUsername(username);
+
+        // Then
+        assertTrue(resultUser.isPresent());
+        assertEquals(username, resultUser.get().getUsername());
+    }
+
+
+}
